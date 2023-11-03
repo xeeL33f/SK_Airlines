e11aebd45cb1dc6661d0f15f2702e429e141ad7c
clownzxy <norwen.penas.21@usjr.edu.ph>
2023-11-03 23:56:09 +0800

    feat: currently implementing travel class

--- Views/FlightBookingPage.xaml.cs
+++ Views/FlightBookingPage.xaml.cs
@@ -7,8 +7,32 @@ public partial class FlightBookingPage : ContentPage
 		InitializeComponent();
 	}
 
-	private async void OnTappedFrame(object sender, TappedEventArgs e)
+	//data should match to the database from the admin based on the route...
+	//for now the default value is this but in the future this should be changed...
+
+    //frame 1
+	private async void OnTappedFrame1(object sender, TappedEventArgs e)
 	{
 		await Navigation.PushAsync(new FlightBookingSelection());
 	}
+
+    //frame2
+    private async void OnTappedFrame2(object sender, TappedEventArgs e)
+    {
+        await Navigation.PushAsync(new FlightBookingSelection());
+    }
+
+
+    //frame3
+    private async void OnTappedFrame3(object sender, TappedEventArgs e)
+    {
+        await Navigation.PushAsync(new FlightBookingSelection());
+    }
+
+
+    //frame4
+    private async void OnTappedFrame4(object sender, TappedEventArgs e)
+    {
+        await Navigation.PushAsync(new FlightBookingSelection());
+    }
 }
