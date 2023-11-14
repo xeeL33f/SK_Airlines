2f460dc0053e71bc1b6875ff869477fead2add78
jak <glowifixs@gmail.com>
2023-11-14 09:13:54 +0800

    ui: Modifying Addons Page

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace SK_Airlines_App
+﻿using SK_Airlines_App.Views;
+
+namespace SK_Airlines_App
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new AddonsPage();
         }
     }
 }
