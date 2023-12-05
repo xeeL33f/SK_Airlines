23409d05f89ad9c169240bbc75a0b8fa1895c171
Jak Ramas <glowifixs@gmail.com>
2023-12-05 08:27:02 +0800

    Commit changes

--- Views/NewLandingPage.xaml.cs
+++ Views/NewLandingPage.xaml.cs
@@ -7,18 +7,4 @@ public partial class NewLandingPage : ContentPage
 		InitializeComponent();
 	}
 
-    private void OnImageTapped(object sender, TappedEventArgs e)
-    {
-        string originValue = originEntry.Text;
-        string destinationValue = destEntry.Text;
-
-        originEntry.Text = destinationValue;
-        destEntry.Text = originValue;
-    }
-
-    private async void OnButtonClicked(object sender, EventArgs e)
-    {
-        await Navigation.PushAsync(new BookingForm());
-
-    }
 }
